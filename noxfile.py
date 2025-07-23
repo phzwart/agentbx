dc46415ab79b2e6f2bf278bc43cc23c5e41bdddf
phzwart <PHZwart@lbl.gov>
2025-07-23 14:03:20 -0700

    add ml group to nox for tests

--- noxfile.py
+++ noxfile.py
@@ -256,6 +256,7 @@ def tests(session: Session) -> None:
     """Run the test suite."""
     session.install(".")
     session.install("coverage[toml]", "pytest", "pygments")
+    session.install(".[ml]")
     try:
         session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
     finally:
