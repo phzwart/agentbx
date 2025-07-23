b09a4aa444ad8585ef13ba957143540a2abc4aac
phzwart <PHZwart@lbl.gov>
2025-07-23 14:05:17 -0700

    add torch to nox for tests

--- noxfile.py
+++ noxfile.py
@@ -256,7 +256,7 @@ def tests(session: Session) -> None:
     """Run the test suite."""
     session.install(".")
     session.install("coverage[toml]", "pytest", "pygments")
-    session.install(".[ml]")
+    session.install("torch")
     try:
         session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
     finally:
